c697a1145002eb80a06603416069c07a5556f7cb
Jessica-Fu <jessiefu33@gmail.com>
2017-11-14 22:30:34 -0600

    First Draft

---
+++ Jessica/PitcoChow/app/src/main/java/com/example/jessica/pitcochow/Visual.java
@@ -0,0 +1,13 @@
+package com.example.jessica.pitcochow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Visual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visual);
+    }
+}
