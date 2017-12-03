761bfa531a19de312426dc791abd672f49594476
Kelly Mack <knmack2@illinois.edu>
2017-12-02 18:32:59 -0600

    making lucky coin thing funner

--- app/src/main/java/edu/illinois/cs465/pictochow/LuckyResult.java
+++ app/src/main/java/edu/illinois/cs465/pictochow/LuckyResult.java
@@ -3,12 +3,15 @@ package edu.illinois.cs465.pictochow;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class LuckyResult extends Activity {
 
+    public TextView title;
+    public TextView chosen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +19,17 @@ public class LuckyResult extends Activity {
         Bundle extras = getIntent().getExtras();
         String[] m = extras.getStringArray("restaurant_names");
         Random r = new Random();
-        String chosen_restaurant = m[r.nextInt(m.length)];
-        TextView chosen = (TextView)findViewById(R.id.chosen);
-        chosen.setText(chosen_restaurant);
+        final String chosen_restaurant = m[r.nextInt(m.length)];
+
+        title = (TextView) findViewById(R.id.getting_random);
+        title.setText("Choosing a restaurant...");
+        chosen = (TextView) findViewById(R.id.chosen);
+
+        Handler handlerTimer = new Handler();
+        handlerTimer.postDelayed(new Runnable(){
+            public void run() {
+                title.setText("You are going to:");
+                chosen.setText(chosen_restaurant);
+            }}, 2000);
     }
 }
